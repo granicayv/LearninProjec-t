d263a79778a56978c129da63b305fcf7656c622a
granicayv <granicayv@gmail.com>
2020-02-02 13:17:54 +0700

    The first commit

---
+++ LearningProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningProject
+//
+//  Created by Юлия Граница on 02.02.2020.
+//  Copyright © 2020 Юлия Граница. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
